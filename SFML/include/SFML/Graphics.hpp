0890e13587f5a8559ecfbe0424f85b68bea9e11e
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-10-07 01:05:39 +0300

    Update Graphics.hpp

--- SFML/include/SFML/Graphics.hpp
+++ SFML/include/SFML/Graphics.hpp
@@ -29,7 +29,7 @@
 // Headers
 ////////////////////////////////////////////////////////////
 
-#include <Window.hpp>
+#include <Window/Window.hpp>
 #include <Graphics/BlendMode.hpp>
 #include <Graphics/CircleShape.hpp>
 #include <Graphics/Color.hpp>
