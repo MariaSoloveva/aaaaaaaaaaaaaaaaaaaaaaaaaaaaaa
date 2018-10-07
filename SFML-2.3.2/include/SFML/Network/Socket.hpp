608c55b2100ccc6c0912c8253aba226721d2dd14
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-10-07 14:36:32 +0300

    Update Socket.hpp

--- SFML-2.3.2/include/SFML/Network/Socket.hpp
+++ SFML-2.3.2/include/SFML/Network/Socket.hpp
@@ -28,9 +28,9 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include <SFML/Network/Export.hpp>
-#include <SFML/Network/SocketHandle.hpp>
-#include <SFML/System/NonCopyable.hpp>
+#include <SFML-2.3.2/include/SFML/Network/Export.hpp>
+#include <SFML-2.3.2/include/SFML/Network/SocketHandle.hpp>
+#include <SFML-2.3.2/include/SFML/System/NonCopyable.hpp>
 #include <vector>
 
 
