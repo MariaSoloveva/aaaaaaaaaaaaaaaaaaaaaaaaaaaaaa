d52393fb44c9343fe782ab10e5fc62886f268db3
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-10-07 16:39:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,34 +3,6 @@
 #include <exception>
 #include <string>
 #include <SFML-2.3.2/include/SFML/Graphics.hpp>
-#include <SFML-2.3.2/include/SFML/Window.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/BlendMode.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/CircleShape.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Color.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/ConvexShape.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Drawable.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Font.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Glyph.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Image.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/PrimitiveType.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Rect.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/RectangleShape.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/RenderStates.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/RenderTarget.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/RenderTexture.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/RenderWindow.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Shader.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Shape.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Sprite.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Text.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Texture.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Transform.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Transformable.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/Vertex.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/VertexArray.hpp>
-#include <SFML-2.3.2/include/SFML/Graphics/View.hpp>
-
-
 
 int main()
 {
