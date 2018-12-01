6525180fc83f6f5f8a54611f14a3f0dcd69d30d8
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-12-01 18:13:31 +0300

    Update TrainAlgorithm.hpp

--- src/classes/TrainAlgorithm.hpp
+++ src/classes/TrainAlgorithm.hpp
@@ -2,7 +2,7 @@
 #define TRAINALGORITHM_H_
 
 #include <vector>
-#include </home/mariasolovyova/CLionProjects/Evolution/tools/json/single_include/nlohmann/json.hpp>
+#include <vendor/json/json.hpp>
 
 using Json = nlohmann::json;
 
